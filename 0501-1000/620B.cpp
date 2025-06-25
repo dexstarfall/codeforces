ef2c5c752d854f46bb430996d0b1edf8764c146b
mintdexdev <dexdevpersona@gmail.com>
2025-06-25 09:50:14 +0530

    2 different approach

--- 0501-1000/620B.cpp
+++ 0501-1000/620B.cpp
@@ -1,62 +1,44 @@
-#include <bits/stdc++.h>
+// sting approach bad approach
 
-#define forn(i, n) for (int i = 0; i < int(n); i++)
-#define ford(i, n) for (int i = int(n) - 1; i >= 0; i--)
-#define fore(i, l, r) for (int i = int(l); i < int(r); i++)
-#define correct(x, y, n, m) (0 <= (x) && (x) < (n) && 0 <= (y) && (y) < (m))
-#define all(a) (a).begin(), (a).end()
-#define sz(a) int((a).size())
-#define pb(a) push_back(a)
-#define mp(x, y) make_pair((x), (y))
-#define x first
-#define y second
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+// 	int a,b; cin >> a >> b;
+// 	int list[] = { 6, 2, 5, 5, 4, 5, 6, 3, 7, 6 };
+
+// 	string str;
+// 	for (int i = a; i <= b ; i++) {
+// 	  str += to_string(i);
+// 	}
+	
+// 	int total = 0;
+// 	for (int i=0; i< str.size(); i++) {
+// 	 int index = (int)str[i]-48;
+// 	  total += list[index];
+// 	}
+	
+// 	cout << total << "\n";
+// }
+
+
+
+//  decent approach
+#include <bits/stdc++.h>
 
 using namespace std;
 
-typedef long long li;
-typedef long double ld;
-typedef pair<int, int> pt;
-
-template<typename X> inline X abs(const X& a) { return a < 0? -a: a; }
-template<typename X> inline X sqr(const X& a) { return a * a; }
-
-const int INF = int(1e9);
-const li INF64 = li(1e18);
-const ld EPS = 1e-9, PI = 3.1415926535897932384626433832795;
-
-int a, b;
-
-inline bool read() {
-	return !!(cin >> a >> b);
-}
-
-int c[] = { 6, 2, 5, 5, 4, 5, 6, 3, 7, 6 };
-
-inline void solve() {
-	int ans = 0;
-	fore(i, a, b + 1) {
-		int x = i;
-		while (x) {
-			ans += c[x % 10];
-			x /= 10;
-		}
-	}
-	cout << ans << endl;
-}
-
 int main() {
-#ifdef SU1
-    assert(freopen("input.txt", "rt", stdin));
-    //assert(freopen("output.txt", "wt", stdout));
-#endif
-    
-    cout << setprecision(10) << fixed;
-    cerr << setprecision(5) << fixed;
+  int a, b;  cin >> a >> b;
+  int segment_list[] = { 6, 2, 5, 5, 4, 5, 6, 3, 7, 6  };
 
-    while (read()) {
-		solve();
-		//break;
-	}
-	
-    return 0;
+  int ans = 0;
+  for (int i = a; i <= b; i++) {
+    int x = i;
+    while (x) {
+      ans += segment_list[x % 10];
+      x /= 10;
+    }
+  }
+  cout << ans;
 }
